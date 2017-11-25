246ee34098a98965c5d5caa40dab651b8fd29f3f
romefeller <matheus.matos@fatec.sp.gov.br>
2017-11-25 16:12:56 +0000

    Arrumando Visual

--- src/Handler/Embarcacao.hs
+++ src/Handler/Embarcacao.hs
@@ -11,12 +11,23 @@ import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 
+
 getEmbarcacaoR :: Handler Html
-getEmbarcacaoR = undefined
-{-    defaultLayout $ do 
+getEmbarcacaoR = undefined {--do
     addStylesheet $ (StaticR css_bootstrap_min_css)
     addScript (StaticR js_bootstrap_min_js)
--}
+    (widget, enctype) <- generateFormPost formEmbarcacao
+    defaultLayout $ do
+        [whamlet|
+            <h1>"Ola Mundo"</h1>
+            <form action =@{EmbarcacaoR} method=post>
+                  ^{widget}
+                 <input type ="submit" value="Cadastrar">
+            --<h2>"Qualquer Coisa"</h2>
+        |]--}
+
+
+
 
 getCadastrarEmbarcacaoR :: Handler Html
 getCadastrarEmbarcacaoR = undefined
--- src/Handler/Marinheiro.hs
+++ src/Handler/Marinheiro.hs
@@ -10,14 +10,42 @@ import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+
+formMarinheiro :: Form Marinheiro
+formMarinheiro = renderBootstrap $ Marinheiro
+    <$> areq textField     "Nome: " Nothing
+    <*> areq emailField    "Email: " Nothing
+    <*> areq passwordField "Senha: "Nothing
+
+
 getMarinheiroR :: Handler Html
 getMarinheiroR = undefined
 
+
 getCadastrarMarinheiroR :: Handler Html
-getCadastrarMarinheiroR = undefined
+getCadastrarMarinheiroR = do
+    (widget, enctype) <- generateFormPost formMarinheiro
+    defaultLayout $ do
+        addStylesheet $ (StaticR css_bootstrap_min_css)
+        addScript (StaticR js_bootstrap_min_js)
+        [whamlet|
+            <form action=@{CadastrarMarinheiroR} method=post>
+                ^{widget}
+                <input type="submit" value="Cadastrar">
+            
+        |]
 
 postCadastrarMarinheiroR :: Handler Html
-postCadastrarMarinheiroR = undefined
+postCadastrarMarinheiroR = do 
+    ((res,_),_) <- runFormPost formMarinheiro
+    case res of 
+        FormSuccess marinheiro -> do
+            _ <- runDB $ insert marinheiro
+            redirect MarinheiroR
+        _ -> do
+            setMessage $ [shamlet| Falha no Cadastro |]
+            redirect CadastrarMarinheiroR
+ 
 
 getListarMarinheiroR :: Handler Html
 getListarMarinheiroR = undefined
